d35107ee5df196b989720e09d23f8390e0739826
4drea4 <bakerand@outlook.com>
2026-01-04 15:29:43 -0600

    updating ip.ts to render users ip

--- ipreact/src/api/ip.ts
+++ ipreact/src/api/ip.ts
@@ -5,7 +5,8 @@ const API_KEY ="at_Dw02MjcDxw7tH0ttIuN7b1nlHrV1J";
 
 //fetch using async function 
 export async function getIpInfo(ip:string): Promise<IpData>{
-    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
+   const baseUrl =`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
+    const url = ip && ip.trim() ? `${baseUrl}&ipAddress=${ip.trim()}`: baseUrl;
     
     const res = await fetch(url);
 
