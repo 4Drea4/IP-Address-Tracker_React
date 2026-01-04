5dc778f42fe1b90ea30952151b2fe30c92d0394a
4drea4 <bakerand@outlook.com>
2026-01-04 14:27:51 -0600

    useeffect for the icon

--- ipreact/src/components/Map.tsx
+++ ipreact/src/components/Map.tsx
@@ -13,6 +13,13 @@ export default function Map({data}:MapProps){
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
     const mapRef = useRef<L.Map | null>(null);
     const markerRef = useRef<L.Marker | null>(null);
-    
+
+    useEffect(() => {
+        L.Icon.Default.mergeOptions({
+            iconRetinaUrl: markerIcon2x,
+            iconUrl: markerIcon,
+            shadowUrl: markerShadow,
+        });
+    }, []);
 
 }
