da017c67ca79a94a81f2418948666e3e66c1626e
4drea4 <bakerand@outlook.com>
2026-01-04 15:38:04 -0600

    troubleshot type error

--- ipreact/src/api/ip.ts
+++ ipreact/src/api/ip.ts
@@ -4,7 +4,7 @@ const API_KEY ="at_Dw02MjcDxw7tH0ttIuN7b1nlHrV1J";
 //I know I shouldnt do this but I cant find a way to hide this rn
 
 //fetch using async function 
-export async function getIpInfo(ip:string): Promise<IpData>{
+export async function getIpInfo(ip?:string): Promise<IpData>{
    const baseUrl =`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
     const url = ip && ip.trim() ? `${baseUrl}&ipAddress=${ip.trim()}`: baseUrl;
     
--- ipreact/src/hooks/useIpData.ts
+++ ipreact/src/hooks/useIpData.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { getIpInfo } from "../api/ip";
-import type { IpData
- } from "../types";
+import type { IpData} from "../types";
+
 
  export function useIpData(){
     //state for error, data and loading
@@ -9,7 +9,7 @@ import type { IpData
     const [data, setData] = useState<IpData | null > (null)
     const [loading, setLoading] = useState(false);
 
-    async function search (ip:string){
+    async function search (ip?:string){
        setLoading(true);
        setError(null) //Reset error and make sure I set loading to true
 
@@ -26,5 +26,5 @@ import type { IpData
     }
     return {
         error, data, loading, search
-    }
+    };
  }
