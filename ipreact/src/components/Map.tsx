71dcc778009a58d7687fd8159a7c9542efa4c02a
4drea4 <bakerand@outlook.com>
2026-01-04 14:24:04 -0600

    setting up tuple for lng and lat, receving data as my prop, dom nodes

--- ipreact/src/components/Map.tsx
+++ ipreact/src/components/Map.tsx
@@ -1,7 +1,18 @@
 import {useEffect, useRef } from 'react';
 import type { IpData } from '../types';
 import L from 'leaflet';
-
+import { MapProps } from '../types';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png' ;
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const defaultCenter: [number, number] = [51.505, -0.09]
+const defaultZoom = 10;
+
+export default function Map({data}:MapProps){
+    const mapContainerRef = useRef<HTMLDivElement | null>(null);
+    const mapRef = useRef<L.Map | null>(null);
+    const markerRef = useRef<L.Marker | null>(null);
+    
+
+}
